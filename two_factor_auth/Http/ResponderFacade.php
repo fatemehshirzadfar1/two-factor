53492daf9c1c0341a8a888c5177d5e185e00e67b
Fatemeh Shirzadfar <shirzadfar88sh@gmail.com>
2022-05-05 11:22:02 +0430

    Update ResponderFacade.php

--- two_factor_auth/Http/ResponderFacade.php
+++ two_factor_auth/Http/ResponderFacade.php
@@ -17,44 +17,6 @@ class ResponderFacade extends Facade
         return VueResponses::class;
     }
 
-//     <?php
-
-// namespace App\Http\Controllers\V1;
-
-// use App\Http\Controllers\Controller;
-
-// use App\ProtectionLayers\FaildeResponses;
-
-// use Illuminate\Support\Facades\Validator;
-
-// class LoginController extends Controller
-// {
-//     public function identifyNationalIdNumber()
-//     {
-//         request('national_id_number');
-//         //CheckUserIsGuest::run();
-//         
-        
-
-//         //find user row in DB or Fail
-//         // $user = UserProviderFacade::getUserByNationalIdNumber($national_id_number)->getOrSend(
-//         //     [FaildeResponses::userNotFound()]
-//         // );
-
-//         // stop block users
-//         // if (UserProviderFacade::isBanned($user->id)) {
-//         //     return ResponderFacade::blockedUser();
-
-//         //if first login redirect to password reset else enter password
-//           //  if (UserProviderFacade::isFirstLogin()) {}
-        
-            
-//         //4.validate password
-//         // $this->validatePasswordIsValid();
-//         //5.create token
-//     }
-// }
-
 }
 
 
